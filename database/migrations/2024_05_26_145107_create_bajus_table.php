1b7e328b366d1a0ac781477338b94787306f0d81
nurul1710 <naja86510@gmail.com>
2024-05-26 22:21:11 +0700

    selesai database

---
+++ database/migrations/2024_05_26_145107_create_bajus_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bajus', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_baju');
+            $table->enum('size', ['XL','L','S','M','XXL']);
+            $table->double('harga_perhari');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bajus');
+    }
+};
---
+++ database/migrations/2024_05_26_145132_create_sewas_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sewas', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal_peminjaman');
+            $table->date('tanggal_pengembalian');
+            $table->double('totalHarga_sewa');
+            $table->BigInteger('customer_id')->unsigned();
+            $table->BigInteger('baju_id')->unsigned();
+            $table->BigInteger('admin_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('sewas', function (Blueprint $table) {
+            $table->foreign('customer_id')->references('id')->on('customers')
+            ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('admin_id')->references('id')->on('admins')
+            ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('baju_id')->references('id')->on('bajus')
+            ->onUpdate('cascade')->onDelete('cascade');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sewas');
+    }
+};
